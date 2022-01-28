e486a1afc388c4089c76f5bac27b5724f98cf0dd
Gabriela Harea <hareagabi@gmail.com>
2022-01-28 14:48:23 +0200

    icon menu

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,20 @@ if (process.env.NODE_ENV !== "production") {
 
 /* Document ready put your JavaScript code here */
 $(() => {});
+
+// const mainMenu = document.querySelector(".header__nav");
+// const closeMenu = document.querySelector(".closeMenu");
+// const openMenu = document.querySelector("openMenu");
+
+// openMenu.addEventListener("click", show);
+// closeMenu.addEventListener("click", close);
+
+// function show() {
+//   mainMenu.style.display = 'flex';
+//   mainMenu.style.top = '0';
+// }
+
+// function close() {
+//   mainMenu.style.top = '-100%';
+// }
+
