ed0d0bdea0f973ee9eec392a6ef020371c24ed18
Gabriela Harea <hareagabi@gmail.com>
2022-01-31 13:14:44 +0200

    eugen cod 31in header mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,11 @@
-/* Import Styles */
-import "../scss/app.scss";
+import './header';
 
-if (process.env.NODE_ENV !== "production") {
-  console.log("Looks like we are in development mode!");
+/* Import Styles */
+import '../scss/app.scss';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
 }
 
 /* Document ready put your JavaScript code here */
 $(() => {});
-
-const mainMenu = document.querySelector(".menu-mobile");
-const openMenu = document.querySelector(".burger");
-const closeMenu = document.querySelector(".x");
-
-openMenu.addEventListener("click", show);
-closeMenu.addEventListener("click", close);
-
-function show() {
-  mainMenu.style.display = "flex";
-  // mainMenu.style.top = '0';
-}
-
-function close() {
-  mainMenu.style.display = "none";
-}
