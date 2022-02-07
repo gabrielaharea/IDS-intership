ec425931d15ce6363b45a3c5735dc4738df12f52
Gabriela Harea <hareagabi@gmail.com>
2022-02-07 12:56:23 +0200

    [DropDown] modify cod Eugen

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,8 +1,19 @@
-button = document.querySelector(".circle");
+// button = document.querySelector(".circle");
 
-const modifyElement = () => {
-  document.querySelector(".vertical").classList.toggle("open");
-  document.querySelector(".text").classList.toggle("open");
-};
+// const modifyElement = () => {
+//   document.querySelector(".vertical").classList.toggle("open");
+//   document.querySelector(".dropdown-description").classList.toggle("open");
+// };
 
-button.addEventListener("click", modifyElement);
+// button.addEventListener("click", modifyElement);
+
+
+const dropdownBtn = document.querySelector('.dropdown-btn');
+let dropdownDescription = document.querySelector('.dropdown-description');
+
+if (dropdownBtn) {
+  dropdownBtn.addEventListener('click', () => {
+    dropdownBtn.classList.toggle('is-active');
+    dropdownDescription.classList.toggle('is-active');
+  });
+}
