be029dfa533eb995b51979c65e5a824bb58ef449
Gabriela Harea <hareagabi@gmail.com>
2022-02-10 11:06:33 +0200

    modify

---
+++ src/js/arrow-left.js
@@ -0,0 +1,13 @@
+// const icon = document.querySelector(".icon");
+// const arrow = document.querySelector(".arrow");
+
+// icon.mouseover = () => {
+//   arrow.animate([{ left: "0" }, { left: "20px" }, { left: "0" }], {
+//     duration: 800,
+//   });
+// };
+
+
+
+
+
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,19 +1,14 @@
-import Swiper from "swiper";
+import Swiper, { Navigation, Pagination } from "swiper";
 
-let Menu = ["Slide 1", "Slide 2", "Slide 3", "Slide 4", "Slide 5"];
-
-const swiper = new Swiper(".swiper", {
+let Menu = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
+var swiper = new Swiper(".swiper", {
   speed: 400,
-  direction: "horizontal",
-  loop: true,
-  pagination: {
-    el: "years-axis",
-    click: true,
-  },
+  direction: "vertical",
+  //   loop: true,
   initialSlide: 0,
   centeredSlides: true,
+  effect: "fade",
+  init: "true",
 });
-
+const mySwiper = document.querySelector(".swiper").swiper;
 let index = document.querySelector(".data-swiper-slide-index");
-
-function changeSlide() {}
