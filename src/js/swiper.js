2c80a204e7b0af029d59d9ba5acbab463e35df28
Eugene Proca <ep@insomniacdesign.com>
2022-02-14 12:48:36 +0200

    [Journey]: Journey Slider: new changes html, js, scss.

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,22 +1,21 @@
-import Swiper, { Navigation, Pagination, EffectFade } from "swiper";
+import Swiper, { Navigation, Pagination, EffectFade } from 'swiper';
 // import $ from 'jquery';
-let Years = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
+let Years = ['1996-2004', '2005-2009', '2010-2014', '2015-2021', '2021-2030'];
 
-var swiper = new Swiper(".journey-slider", {
+var swiper = new Swiper('.journey-slider', {
   modules: [Pagination, Navigation, EffectFade],
-  effect: "fade",
+  wrapperClass: 'journey-container',
+  effect: 'fade',
   speed: 250,
   fadeEffect: {
     crossFade: true,
   },
 
   pagination: {
-    el: ".swiper-pagination",
-    // type: 'bullets',
+    el: '.journey-pagination',
     clickable: true,
-    // hideOnClick: true,
-    bulletClass: "journey-bullet",
-    bulletActiveClass: "is-active",
+    bulletClass: 'journey-bullet',
+    bulletActiveClass: 'is-active',
     renderBullet: function (index, className) {
       return `<button type="button" class="${className}">${Years[index]}</button>`;
     },
