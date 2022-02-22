8cc31746c7aa7786ac4439cfe79ac729308cab04
Gabriela Harea <hareagabi@gmail.com>
2022-02-22 11:19:04 +0200

    modify footer

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,24 +1,28 @@
 import Swiper, { Autoplay, Navigation, Pagination, EffectFade } from "swiper";
 
-let Years = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
+const renderYears = [
+  "1966 - 2004",
+  "2005 – 2009",
+  "2010 - 2014",
+  "2015 - 2021",
+  "2021 - 2030",
+];
 
-var swiper = new Swiper(".journey-slider", {
+const knowledgeSlider = new Swiper(".knowledge-slider", {
   modules: [Pagination, Navigation, EffectFade],
-  wrapperClass: "journey-container",
-  effect: "fade",
-  speed: 250,
-  fadeEffect: {
-    crossFade: true,
+  autoplay: {
+    delay: 6700,
+    waitForTransition: false,
+    pauseOnMouseEnter: true,
   },
-
+  height: 300,
+  effect: "fade",
+  fadeEffect: { crossFade: true },
   pagination: {
-    el: ".journey-pagination",
+    el: ".swiper-pagination",
     clickable: true,
-    bulletClass: "journey-bullet",
-    bulletActiveClass: "is-active",
-    renderBullet: function (index, className) {
-      return `<button type="button" class="${className}">${Years[index]}</button>`;
-    },
+    renderBullet: (index, className) =>
+      `<span class="${className}">${renderYears[index]}</span>`,
   },
 });
 
